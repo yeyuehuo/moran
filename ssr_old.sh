0436802463e1d1edfd9b476bc2107e51475d0e94
yeyuehuo <45181320+yeyuehuo@users.noreply.github.com>
2019-05-19 22:31:32 +0800

    Update ssr_old.sh

--- ssr_old.sh
+++ ssr_old.sh
@@ -868,7 +868,7 @@ JQ_install(){
 	JQ_ver=`jq -V`
 	if [[ -z ${JQ_ver} ]]; then
 		#wget --no-check-certificate -N "https://softs.pw/Bash/other/jq-1.5.tar.gz"
-		wget --no-check-certificate -N "https://raw.githubusercontent.com/moranchenxi/moran/master/other/jq-1.5.tar.gz"
+		wget --no-check-certificate -N "https://raw.githubusercontent.com/yeyuehuo/moran/master/other/jq-1.5.tar.gz"
 		tar -xzf jq-1.5.tar.gz && cd jq-1.5
 		./configure --disable-maintainer-mode && make && make install
 		ldconfig
@@ -1514,7 +1514,7 @@ UninstallServerSpeeder(){
 	fi
 }
 BanBTPTSPAM(){
-	wget -4qO- raw.githubusercontent.com/moranchenxi/moran/master/Get_Out_Spam.sh | bash
+	wget -4qO- raw.githubusercontent.com/yeyuehuo/moran/master/Get_Out_Spam.sh | bash
 }
 InstallBBR(){
 	echo -e "${Info_install_bbr_0}"
@@ -1523,7 +1523,7 @@ InstallBBR(){
 	stty erase '^H' && read -p "(${Word_default}: ${Word_cancel}):" unyn
 	[[ -z ${unyn} ]] && echo "${Word_canceled}" && exit 1
 	if [[ ${unyn} == [Yy] ]]; then
-		wget -N --no-check-certificate https://softs.pw/Bash/bbr.sh && chmod +x bbr.sh && bash bbr.sh
+		wget -N --no-check-certificate https://raw.githubusercontent.com/yeyuehuo/moran/master/bbr.sh/ && chmod +x bbr.sh && bash bbr.sh
 	fi
 }
 SetCrontab_interval(){
